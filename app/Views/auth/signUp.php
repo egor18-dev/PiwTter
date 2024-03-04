3965fdd0055d5869264ce943cf5886d2e4ae4947
Egor Santamaria <esantamaria@vunkers.com>
2024-03-04 09:29:33 +0100

    Formulari crear compte acabat

--- app/Views/auth/signUp.php
+++ app/Views/auth/signUp.php
@@ -1,20 +1,20 @@
-<?php
-
-    echo form_open('create_user.php', ['id' => 'frmUsers']);
-
-    echo form_input(['type' => 'text', 'name' => 'name']);
-
-    echo form_input(['type' => 'email', 'name' => 'email']);
-
-    echo form_input(['type' => 'password', 'name' => 'password']);
-
-    echo form_submit('btnSubmit', 'Registrar');
-
-
-
-
-?>
-
-<?php
-    echo form_close();
-?>
+<?= $this->extend('main') ?>
+<?=$this->section('content')?>
+    <div class="container-sm p-5 border border-gray rounded mt-5">
+        <h5 class="fw-normal text-uppercase mb-3">Crea el teu compte</h5>
+        <?=form_open('create_user.php', ['id' => 'frmUsers'])?>
+        <div class="form-group">
+            <?=form_input(['type' => 'text', 'name' => 'name', 'class' => 'form-control'])?>
+        </div>
+        <div class="form-group mt-3">
+            <?=form_input(['type' => 'email', 'name' => 'email', 'class' => 'form-control'])?>
+        </div>
+        <div class="form-group mt-3">
+            <?=form_input(['type' => 'password', 'name' => 'password', 'class' => 'form-control'])?>
+        </div>
+        <div class="form-group mt-3">
+            <?=form_submit('btnSubmit', 'Registrar', ['class' => 'btn btn-outline-dark'])?>
+        </div>
+        <?=form_close()?>
+    </div>
+<?=$this->endSection()?>
