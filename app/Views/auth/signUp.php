f1c563130885d1dcf1ce321977b33df4f746e995
Egor Santamaria <esantamaria@vunkers.com>
2024-03-04 09:31:12 +0100

    Placeholders

--- app/Views/auth/signUp.php
+++ app/Views/auth/signUp.php
@@ -4,13 +4,13 @@
         <h5 class="fw-normal text-uppercase mb-3">Crea el teu compte</h5>
         <?=form_open('create_user.php', ['id' => 'frmUsers'])?>
         <div class="form-group">
-            <?=form_input(['type' => 'text', 'name' => 'name', 'class' => 'form-control'])?>
+            <?=form_input(['type' => 'text', 'name' => 'name', 'class' => 'form-control', 'placeholder' => 'Introdueix el teu nom'])?>
         </div>
         <div class="form-group mt-3">
-            <?=form_input(['type' => 'email', 'name' => 'email', 'class' => 'form-control'])?>
+            <?=form_input(['type' => 'email', 'name' => 'email', 'class' => 'form-control', 'placeholder' => 'Introdueix el teu correu electrònic'])?>
         </div>
         <div class="form-group mt-3">
-            <?=form_input(['type' => 'password', 'name' => 'password', 'class' => 'form-control'])?>
+            <?=form_input(['type' => 'password', 'name' => 'password', 'class' => 'form-control', 'placeholder' => 'Introdueix la teva contrasenya'])?>
         </div>
         <div class="form-group mt-3">
             <?=form_submit('btnSubmit', 'Registrar', ['class' => 'btn btn-outline-dark'])?>
