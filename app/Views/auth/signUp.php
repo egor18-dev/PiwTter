40438542a1ac2e155f709d75832e0cb5054905f6
Egor Santamaria <esantamaria@vunkers.com>
2024-03-04 09:37:14 +0100

    Redireccioó

--- app/Views/auth/signUp.php
+++ app/Views/auth/signUp.php
@@ -3,16 +3,14 @@
     <div class="container-sm p-5 border border-gray rounded mt-5">
         <h5 class="fw-normal text-uppercase mb-3">Crea el teu compte</h5>
         <?=form_open('create_user.php', ['id' => 'frmUsers'])?>
-        <div class="form-group">
-            <?=form_input(['type' => 'text', 'name' => 'name', 'class' => 'form-control', 'placeholder' => 'Introdueix el teu nom'])?>
-        </div>
         <div class="form-group mt-3">
-            <?=form_input(['type' => 'email', 'name' => 'email', 'class' => 'form-control', 'placeholder' => 'Introdueix el teu correu electrònic'])?>
+            <?=form_input(['type' => 'text', 'name' => 'user', 'class' => 'form-control', 'placeholder' => 'Introdueix el teu usuari'])?>
         </div>
         <div class="form-group mt-3">
             <?=form_input(['type' => 'password', 'name' => 'password', 'class' => 'form-control', 'placeholder' => 'Introdueix la teva contrasenya'])?>
         </div>
         <div class="form-group mt-3">
+            <h6 class="fw-normal mb-3">Ja tens usuari? <a class="fw-bold">Inicia sesió</a></h6>
             <?=form_submit('btnSubmit', 'Registrar', ['class' => 'btn btn-outline-dark'])?>
         </div>
         <?=form_close()?>
