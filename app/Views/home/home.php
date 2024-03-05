55ef16a3f8ed49ba2c4a72470cade9634f709f45
Egor Santamaria <esantamaria@vunkers.com>
2024-03-05 10:04:19 +0100

    Nous estils

--- app/Views/home/home.php
+++ app/Views/home/home.php
@@ -1,32 +1,40 @@
 <?= $this->extend('main') ?>
 <?=$this->section('content')?>
+<div class="container-fluid d-flex align-items-center justify-content-center p-5" 
+    style="background-image: url('https://images5.alphacoders.com/113/1138652.jpg');
+    background-position: center;
+    background-size: cover;
+    background-attachment: fixed;
+    height: 30vh;">
+    <h1 class="fw-normal text-uppercase text-white">Piulades</h1>
+</div>
 <div class="container-fluid p-5">
     <div class="row align-items-center justify-content-center">
-        <div class="col-lg-3 overflow-hidden border border-dark rounded p-3 m-3">
+        <div class="col-lg-3 overflow-hidden border border-dark rounded p-3 m-2">
             <p>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem blanditiis repellendus quos aspernatur inventore, sit nulla sint vero sequi, accusantium consequuntur eligendi animi architecto adipisci doloremque voluptates deserunt, ipsa provident?
             </p>
         </div>
       
-        <div class="col-lg-3 overflow-hidden border border-dark rounded p-3 m-3">
+        <div class="col-lg-3 overflow-hidden border border-dark rounded p-3 m-2">
             <p>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem blanditiis repellendus quos aspernatur inventore, sit nulla sint vero sequi, accusantium consequuntur eligendi animi architecto adipisci doloremque voluptates deserunt, ipsa provident?
             </p>
         </div>
       
-        <div class="col-lg-3 overflow-hidden border border-dark rounded p-3 m-3">
+        <div class="col-lg-3 overflow-hidden border border-dark rounded p-3 m-2">
             <p>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem blanditiis repellendus quos aspernatur inventore, sit nulla sint vero sequi, accusantium consequuntur eligendi animi architecto adipisci doloremque voluptates deserunt, ipsa provident?
             </p>
         </div>
       
-        <div class="col-lg-3 overflow-hidden border border-dark rounded p-3 m-3">
+        <div class="col-lg-3 overflow-hidden border border-dark rounded p-3 m-2">
             <p>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem blanditiis repellendus quos aspernatur inventore, sit nulla sint vero sequi, accusantium consequuntur eligendi animi architecto adipisci doloremque voluptates deserunt, ipsa provident?
             </p>
         </div>
       
-        <div class="col-lg-3 overflow-hidden border border-dark rounded p-3 m-3">
+        <div class="col-lg-3 overflow-hidden border border-dark rounded p-3 m-2">
             <p>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem blanditiis repellendus quos aspernatur inventore, sit nulla sint vero sequi, accusantium consequuntur eligendi animi architecto adipisci doloremque voluptates deserunt, ipsa provident?
             </p>
