c28d1983e4722b6c157a973f4f5f4f6712a09b53
Egor Santamaria <esantamaria@vunkers.com>
2024-03-11 08:51:55 +0100

    Ckeditor form

--- app/Views/home/add.php
+++ app/Views/home/add.php
@@ -4,7 +4,18 @@
     <div class="container-sm min-vh-100 d-flex flex-column align-items-center justify-content-center">
         <form action="#" class="border border-dark rounded p-3 w-100">
             <h5>Afegir publicació</h5>
+            <textarea id="ID_TEXTAREA"></textarea>
         </form>
     </div>
+
+    <script src="<?= base_url('assets/ckeditor/build/ckeditor.js') ?>"></script>
+
+    <script>
+        ClassicEditor
+            .create( document.querySelector( '#ID_TEXTAREA' ) )
+            .catch( error => {
+                console.error( error );
+            } );
+    </script>
 
 <?=$this->endSection()?>
