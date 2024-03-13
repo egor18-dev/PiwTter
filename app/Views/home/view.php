6c139b6993b0f38f08a767333d91660c1476c2b0
Egor Santamaria <esantamaria@vunkers.com>
2024-03-13 10:50:45 +0100

    Tot bé

--- app/Views/home/view.php
+++ app/Views/home/view.php
@@ -3,6 +3,7 @@
     <div class="row align-items-center justify-content-center mt-5">
         <div class="col-lg-6 border border-dark rounded p-5">
             <?=$post?>
+            <a href="/home" class="btn btn-outline-dark mt-3">Tornar</a>
         </div>
     </div>
 <?= $this->endSection() ?>
