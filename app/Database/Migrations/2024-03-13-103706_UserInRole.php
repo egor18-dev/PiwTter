1cf2b16cf190de932f43aa6333cf3ad4b56f106e
Egor Santamaria <esantamaria@vunkers.com>
2024-03-13 12:00:01 +0100

    Database preparada

---
+++ app/Database/Migrations/2024-03-13-103706_UserInRole.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class UserRoles extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id' => [
+                'type' => 'INT',
+                'constraint' => 11,
+                'auto_increment' => true,
+            ],
+            'user_id' => [
+                'type' => 'INT',
+                'constraint' => 1,
+                'unsigned'       => true,
+            ],
+            'role_id' => [
+                'type' => 'INT',
+                'constraint' => 11,
+            ],
+        ]);
+        $this->forge->addKey('id', true);
+        $this->forge->addForeignKey('user_id', 'users', 'user_id', 'CASCADE', 'CASCADE');
+        $this->forge->addForeignKey('role_id', 'roles', 'id', 'CASCADE', 'CASCADE');
+        $this->forge->createTable('user_roles');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('user_roles');
+    }
+}
--- app/Database/Seeds/RolePermissionsSeeder.php
+++ app/Database/Seeds/RolePermissionsSeeder.php
@@ -8,17 +8,13 @@ class RolePermissionsSeeder extends Seeder
 {
     public function run()
     {
-        // Obtener IDs de roles y permisos desde la base de datos
         $roles = $this->db->table('roles')->select('id')->get()->getResult();
         $permissions = $this->db->table('permissions')->select('id')->get()->getResult();
 
-        // Array para almacenar datos a insertar en la tabla pivot
         $data = [];
 
-        // Iterar sobre los roles y permisos para generar combinaciones
         foreach ($roles as $role) {
             foreach ($permissions as $permission) {
-                // Añadir combinación a los datos
                 $data[] = [
                     'role_id' => $role->id,
                     'permission_id' => $permission->id
